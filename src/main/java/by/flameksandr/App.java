346a94fa38cdfe18fb840a9c04c46e2af0614790
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-26 13:42:53 +0300

    read from DB

--- src/main/java/by/flameksandr/App.java
+++ src/main/java/by/flameksandr/App.java
@@ -1,12 +1,34 @@
 package by.flameksandr;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class App {
     public static void main(String[] args) {
         try (SessionFactory sessionFactory = new Configuration()
+                .addAnnotatedClass(Item.class)
                 .buildSessionFactory()) {
+
+            Session session = null;
+//            //CREATE
+//            session = sessionFactory.getCurrentSession();
+//            session.beginTransaction();
+//
+//            Item item = new Item("Milk", 80);
+//            System.out.println(item);
+//            session.persist(item);
+//            System.out.println(item);
+
+            //READ
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            Item itemFromDb = session.get(Item.class, 1L);
+            System.out.println(itemFromDb);
+
+            session.getTransaction().commit();
+
+
         }
     }
 }

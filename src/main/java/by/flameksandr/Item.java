d10073d3aa22361911fa4a4c59e89eb40bf2f2cb
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-26 13:27:31 +0300

    create item

---
+++ src/main/java/by/flameksandr/Item.java
@@ -0,0 +1,48 @@
+package by.flameksandr;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "price")
+    private int price;
+
+    public Item() {
+    }
+
+    public Item(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
